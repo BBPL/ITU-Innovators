e30c97b6b90eb474830b5b82f95a76fb90928c53
Frem <frem@itu.dk>
2019-04-10 17:50:48 +0200

    button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Button from '../components/Button/Button'
 
 export default class IndexPage extends React.Component {
   render() {
