2070493e952a3933d41bc60b02eddc1be9af52f8
DESKTOP-0NHS4TH\Thibaud <thibaud.manier@gmail.com>
2019-04-11 18:28:27 +0200

    [BOOM] panels are working

--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -4,10 +4,10 @@ import "./Panel.sass";
 
 const Panel = ({ panel }) => (
   <div>
-    {console.log(JSON.stringify(panel[0]))}
+    {console.log(JSON.stringify(panel))}
     <div id="page-container">
-      <div>
-        {panel[0].titles.map((title, index) => (
+      <div className='pan'>
+        {panel.map((title, index) => (
           <ul key={index}>
             <li>{title.title}</li>
             <li>{title.content} </li>
